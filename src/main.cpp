da0959e320cf09e15ca0735827a6ad126c87faf3
dioniDR <129230048+dioniDR@users.noreply.github.com>
2025-01-02 19:26:01 -0700

    con servidor y botones

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,63 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-const int ledPin = 2; // Pin GPIO2 donde está conectado el LED
+const char* ssid = "Pixel_9615";
+const char* password = "hsx2geazxcwufy5";
+
+ESP8266WebServer server(80);
+
+const int ledPins[] = {2, 5, 4, 0}; // Pines GPIO donde están conectados los LEDs
+
+void handleRoot() {
+    String html = "<html><body><h1>Control de LEDs</h1>";
+    for (int i = 0; i < 4; i++) {
+        html += "<p>LED " + String(i+1) + ": <a href=\"/on" + String(i) + "\">ON</a> <a href=\"/off" + String(i) + "\">OFF</a></p>";
+    }
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+}
+
+void handleLEDOn(int ledIndex) {
+    digitalWrite(ledPins[ledIndex], HIGH);
+    handleRoot();
+}
+
+void handleLEDOff(int ledIndex) {
+    digitalWrite(ledPins[ledIndex], LOW);
+    handleRoot();
+}
 
 void setup() {
-    pinMode(ledPin, OUTPUT); // Configura el pin GPIO2 como salida
+    Serial.begin(115200);
+    for (int i = 0; i < 4; i++) {
+        pinMode(ledPins[i], OUTPUT);
+        digitalWrite(ledPins[i], LOW);
+    }
+
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    digitalWrite(ledPins[0], HIGH); // Enciende el LED en GPIO2 al conectarse
+
+    server.on("/", handleRoot);
+    for (int i = 0; i < 4; i++) {
+        server.on("/on" + String(i), [i]() { handleLEDOn(i); });
+        server.on("/off" + String(i), [i]() { handleLEDOff(i); });
+    }
+
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 void loop() {
-    digitalWrite(ledPin, HIGH); // Enciende el LED
-    delay(1000);                // Espera 1 segundo
-    digitalWrite(ledPin, LOW);  // Apaga el LED
-    delay(1000);                // Espera 1 segundo
+    server.handleClient();
 }
